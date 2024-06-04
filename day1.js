33b1f92f52ce8a2e67035416ae84c91e958eb428
aadil10i <aadil3003@outlook.com>
2024-06-04 16:24:22 +0400

    removed

--- day1.js
+++
@@ -1,9 +0,0 @@
-const input = require('./input.txt');
-const up = '(';
-const down = ')';
-
-let final_floor = input;
-
-for (let i = 0; i < instructions; ++i) {
-  if (input === up) return instructions;
-}
