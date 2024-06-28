2e859249ae9b73b8611109f03dc2d1f3a303c3c1
aadil10i <aadil3003@outlook.com>
2024-06-28 23:13:32 +0400

    day 5 part 1 final

--- 2020/5/part-one.ts
+++ 2020/5/part-one.ts
@@ -15,37 +15,43 @@ module.exports = {
 const totalRows = 128;
 const totalColumns = 8;
 
-console.log(input[0]);
-
 // first extract row and column characters from each boarding pass
-function decodeSeat(input) {
+function decodeSeat(pass) {
   let rowLow = 0;
   let rowHigh = totalRows - 1;
   let colLow = 0;
   let colHigh = totalColumns - 1;
 
-  for (let pass of input) {
-    // let row = pass.substring(0, 7);
-    // let column = pass.substring(7, 10);
-
-    for (let i = 0; i < 7; i++) {
-      const rowMid = Math.floor((rowLow - rowHigh) / 2);
-      if (pass[i] === 'F') {
-        rowHigh = rowMid;
-      } else {
-        rowLow = rowMid + 1;
-      }
-    }
-
-    for (let i = 7; i < 10; i++) {
-      const colMid = Math.floor((colLow - colHigh) / 2);
-      if (pass[i] === 'L') {
-        colHigh = colMid;
-      } else {
-        colLow = colMid + 1;
-      }
+  for (let i = 0; i < 7; i++) {
+    const rowMid = Math.floor((rowLow + rowHigh) / 2);
+    if (pass[i] === 'F') {
+      rowHigh = rowMid;
+    } else {
+      rowLow = rowMid + 1;
     }
   }
+
+  for (let i = 7; i < 10; i++) {
+    const colMid = Math.floor((colLow + colHigh) / 2);
+    if (pass[i] === 'L') {
+      colHigh = colMid;
+    } else {
+      colLow = colMid + 1;
+    }
+  }
+  return rowLow * 8 + colLow; // Directly return the seatID
 }
 
-decodeSeat(input);
+function findHighestSeatID(input) {
+  let highestSeatId = 0;
+  for (let pass of input) {
+    const seatId = decodeSeat(pass);
+
+    if (seatId > highestSeatId) {
+      highestSeatId = seatId;
+    }
+  }
+  return highestSeatId;
+}
+
+console.log('highest seat id', findHighestSeatID(input));
