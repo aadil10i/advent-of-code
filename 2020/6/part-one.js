501629204c6643f60205ea61f9c5fe329b3f4918
aadil10i <aadil3003@outlook.com>
2024-07-07 21:19:03 +0400

    day 6 part 1 start

--- 2020/6/part-one.js
+++ 2020/6/part-one.js
@@ -4,11 +4,27 @@ const fs = require('fs');
 const input = fs
   .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
   .toString()
-  .trim()
-  .split('\n');
+  .trim();
 
 module.exports = {
   input,
 };
 
-console.log(input);
+function countUnique(group) {
+  const uniqueLetters = new Set(group.replace(/\n/g, '').split(''));
+  return uniqueLetters.size;
+}
+
+function findAnswers(input) {
+  const groups = input.split('\n\n');
+  let totalSum = 0;
+
+  for (let group of groups) {
+    const uniqueCount = countUnique(group);
+    totalSum += uniqueCount;
+  }
+
+  console.log('total sum of unique answers is', totalSum);
+}
+
+findAnswers(input);
