0e5ff5bde0d57049d2c722b87b16a56530e49dbb
aadil10i <aadil3003@outlook.com>
2024-07-13 16:23:42 +0400

    day 6 part two

---
+++ 2020/6/part-two.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const fs = require('fs');
+
+const input = fs
+  .readFileSync(path.join(__dirname, 'input.txt'), 'utf8')
+  .toString()
+  .trim();
+
+module.exports = {
+  input,
+};
+
+function countUnique(group) {
+  const uniqueLetters = new Set(group.replace(/\n/g, '').split(''));
+  return uniqueLetters.size;
+}
+
+function findAnswers(input) {
+  const groups = input.split('\n\n');
+  let totalSum = 0;
+
+  for (let group of groups) {
+    const uniqueCount = countUnique(group);
+    totalSum += uniqueCount;
+  }
+
+  console.log('total sum of unique answers is', totalSum);
+}
+
+findAnswers(input);
