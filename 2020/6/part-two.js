f92f1cf764e1b2ebc70425c7905c7a7d0fb673c8
aadil10i <aadil3003@outlook.com>
2024-07-20 14:15:17 +0400

    day 6 part two complete

--- 2020/6/part-two.js
+++ 2020/6/part-two.js
@@ -13,6 +13,26 @@ module.exports = {
 function countUnique(group) {
   const uniqueLetters = new Set(group.replace(/\n/g, '').split(''));
   return uniqueLetters.size;
+
+
+function countCommonLetters(group) {
+  const lines = group.split('\n');
+  if (lines.length === 1) {
+    // Single line: Calculate unique letters
+    return countUnique(group);
+  } else {
+    // Multiple lines: Find common letters
+    const commonLetters = new Set(lines[0]);
+    for (let i = 1; i < lines.length; i++) {
+      const lineLetters = new Set(lines[i]);
+      commonLetters.forEach(letter => {
+        if (!lineLetters.has(letter)) {
+          commonLetters.delete(letter);
+        }
+      });
+    }
+    return commonLetters.size;
+  }
 }
 
 function findAnswers(input) {
@@ -20,11 +40,11 @@ function findAnswers(input) {
   let totalSum = 0;
 
   for (let group of groups) {
-    const uniqueCount = countUnique(group);
-    totalSum += uniqueCount;
+    const commonCount = countCommonLetters(group);
+    totalSum += commonCount;
   }
 
-  console.log('total sum of unique answers is', totalSum);
+  console.log('Total sum of common answers:', totalSum);
 }
 
 findAnswers(input);
